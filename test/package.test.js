b7d0fa31eb28976870a6213dab37ca4c12a743f0
fsubal <subal@pixiv.co.jp>
2022-09-21 23:28:46 +0900

    stop using commonjs

--- test/package.test.js
+++ test/package.test.js
@@ -1,12 +1,12 @@
-import strategy, { Strategy } from "..";
+import { PixivStrategy } from "..";
 
 describe("passport-pixiv", () => {
   it("should export Strategy constructor directly from package", () => {
-    expect(strategy).toBeInstanceOf(Function);
+    expect(PixivStrategy).toBeInstanceOf(Function);
     // expect(strategy).to.equal(strategy.Strategy);
   });
 
-  it("should export Strategy constructor", () => {
-    expect(Strategy).toBeInstanceOf(Function);
+  it.skip("should export Strategy constructor", () => {
+    // expect(Strategy).toBeInstanceOf(Function);
   });
 });
